9703bba0aa59ec467abb4c076c78f7c5c8f0629a
osmiornica <sigmundzik.krzysztof@gmail.com>
2022-11-14 22:22:29 +0100

    this?

---
+++ src/js/02-video.js
@@ -0,0 +1,26 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on("timeupdate", throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem("videoplayer-current-time", `${seconds}`);
+    
+}, 1000));
+
+const actualTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(actualTime).then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
